50ed520dcf23b805787c8bdd1670623e62c1f1db
Craig Mayhew <craig@mayhew.io>
2020-01-04 13:00:25 +0000

    Added test for multiple speakers, save as, specified tmp dir

--- test_tscribe.py
+++ test_tscribe.py
@@ -50,6 +50,31 @@ def test_multiple_speakers_with_save_as():
     os.remove("chart.png")
 
 
+def test_multiple_speakers_with_save_as_with_tmp_dir():
+    """
+    Test output exists with multiple speaker input, and save_as defined, and tmp_dir defined
+
+    # GIVEN a sample file containing multiple speakers, and an output filename, and a writable tmp directory
+    # WHEN calling tscribe.write(...)
+    # THEN produce the .docx, named correctly, without errors
+
+    """
+
+    # Setup
+    input_file = "sample_multiple.json"
+    output_file = "test_sample.docx"
+    tmp_dir = "/tmp/"
+    assert os.access(input_file, os.F_OK), "Input file not found"
+
+    # Function
+    tscribe.write(input_file, save_as=output_file, tmp_dir=tmp_dir)
+    assert os.access(output_file, os.F_OK), "Output file not found"
+
+    # Teardown
+    os.remove(output_file)
+    os.remove(tmp_dir+"chart.png")
+
+
 def test_single_speaker():
     """
     Test output exists with single speaker input
