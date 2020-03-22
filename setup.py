d628f007caee7a03447e6e148d2a3a657b1a577b
Bob <robertedwardwilliams@me.com>
2020-03-22 00:47:05 +0000

    Updates pypi supported versions

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
