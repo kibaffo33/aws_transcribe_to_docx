a0ef105793bc815ab2a7b99fb7890fa9971a1afc
Bob <robertedwardwilliams@me.com>
2020-03-27 11:18:25 +0000

    Release of 1.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="tscribe",
-    version="1.2.2",
+    version="1.2.3",
     author="Robert Williams",
     author_email="robertedwardwilliams@me.com",
     description="Produce Word Document, CSV or SQLite transcriptions using the automatic speech recognition from AWS Transcribe.",
